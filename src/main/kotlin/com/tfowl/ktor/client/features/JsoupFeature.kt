da8825bdf9005b1a707a36ccb9c254129c677fa6
Thomas <thomasjamesfowler97@gmail.com>
2019-05-25 18:28:10 +1000

    Added Kdoc and a utility extension function for HttpClientConfig

--- src/main/kotlin/com/tfowl/ktor/client/features/JsoupFeature.kt
+++ src/main/kotlin/com/tfowl/ktor/client/features/JsoupFeature.kt
@@ -1,6 +1,7 @@
 package com.tfowl.ktor.client.features
 
 import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
 import io.ktor.client.features.HttpClientFeature
 import io.ktor.client.response.HttpResponseContainer
 import io.ktor.client.response.HttpResponsePipeline
@@ -13,14 +14,40 @@ import org.jsoup.nodes.Document
 import org.jsoup.parser.Parser
 import kotlin.reflect.full.isSuperclassOf
 
-class JsoupFeature(val parsers: Map<ContentType, Parser>) {
+/**
+ * [HttpClient] feature that deserialises response bodies into Jsoup's [Document]
+ * class using a provided [Parser]
+ *
+ * By default [Html][ContentType.Text.Html] is deserialised using [Parser.htmlParser]
+ * and [Xml][ContentType.Text.Xml] is deserialised using [Parser.xmlParser].
+ *
+ * Note: It will only deserialised registered content types and for receiving
+ * [Document] or superclasses.
+ *
+ * @property parsers Registered parsers for content types
+ */
+class JsoupFeature internal constructor(val parsers: Map<ContentType, Parser>) {
 
+    /**
+     * [JsoupFeature] configuration that is used during installation
+     */
     class Config {
+
+        /**
+         * [Parsers][Parser] that will be used for each [ContentType]
+         *
+         * Default registered are:
+         *  - [Html][ContentType.Text.Html]
+         *  - [Xml][ContentType.Text.Xml]
+         */
         var parsers = mutableMapOf(
                 ContentType.Text.Html to Parser.htmlParser(),
                 ContentType.Text.Xml to Parser.xmlParser())
     }
 
+    /**
+     * Companion object for feature installation
+     */
     companion object Feature : HttpClientFeature<Config, JsoupFeature> {
         override val key: AttributeKey<JsoupFeature> = AttributeKey("Jsoup")
 
@@ -44,4 +71,11 @@ class JsoupFeature(val parsers: Map<ContentType, Parser>) {
             }
         }
     }
+}
+
+/**
+ * Install [JsoupFeature]
+ */
+fun HttpClientConfig<*>.Jsoup(block: JsoupFeature.Config.() -> Unit) {
+    install(JsoupFeature, block)
 }
