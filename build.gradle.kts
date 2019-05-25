18be85cc3c05bfe0f9026abc4980ff68342a7055
Thomas <thomasjamesfowler97@gmail.com>
2019-05-25 19:17:58 +1000

    Overhauled the gradle build script. Included publishing, kdoc generation, testing config, wrapper version specification, new dependency configurations,

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.dokka.gradle.*
+import tanvd.kosogor.proxy.publishJar
+import java.net.URL
 
 plugins {
-    java
     kotlin("jvm") version Versions.kotlin
+    id("org.jetbrains.dokka") version Versions.dokka
+    id("tanvd.kosogor") version Versions.kosogor
 }
 
 java {
@@ -14,6 +20,7 @@ tasks.withType<KotlinCompile>().all {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+
 repositories {
     jcenter()
     mavenCentral()
@@ -21,16 +28,67 @@ repositories {
 }
 
 dependencies {
-    compile(Libraries.kotlin.stdlibJdk8)
-    
+    api(Libraries.kotlin.stdlibJdk8)
+
     with(Libraries.ktor) {
-        compile(client)
-        testCompile(clientMockJvm)
+        api(client)
+        testImplementation(clientMockJvm)
     }
 
-    compile(Libraries.jsoup)
+    api(Libraries.jsoup)
 
-    testCompile(Libraries.junit)
-    testCompile(Libraries.mockk)
+    testImplementation(Libraries.junit)
+    testImplementation(Libraries.mockk)
 }
 
+tasks.withType<Test>().all {
+    jvmArgs = listOf("-XX:MaxPermSize=256m")
+    testLogging {
+        events.addAll(listOf(
+                TestLogEvent.PASSED,
+                TestLogEvent.FAILED,
+                TestLogEvent.SKIPPED
+        ))
+        showStandardStreams = true
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
+
+tasks.withType<DokkaTask>().all {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+
+    externalDocumentationLink {
+        url = URL("https://jsoup.org/apidocs/")
+    }
+
+    externalDocumentationLink {
+        url = URL("https://api.ktor.io/${Libraries.ktor.version}/")
+    }
+}
+
+
+publishJar {
+
+    publication {
+        artifactId = "ktor-jsoup"
+    }
+
+    bintray {
+        username = System.getenv("BINTRAY_USER")
+        secretKey = System.getenv("BINTRAY_KEY")
+
+        repository = "ktor"
+
+        info {
+            githubRepo = "https://github.com/T-Fowl/ktor-jsoup.git"
+            vcsUrl = "https://github.com/T-Fowl/ktor-jsoup.git"
+            license = "MIT"
+        }
+    }
+}
+
+tasks.withType<Wrapper>().all {
+    gradleVersion = "5.4.1"
+    distributionType = Wrapper.DistributionType.ALL
+}
