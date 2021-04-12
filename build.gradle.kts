a3219d4c34567613b87c08c23574959f4f3b193e
Thomas <thomasjamesfowler97@gmail.com>
2021-04-12 17:49:30 +1000

    Removed unused & deprecated repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,8 @@ tasks.withType<KotlinCompile>().all {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-
 repositories {
-    jcenter()
     mavenCentral()
-    maven(url = Repositories.Kotlin.kotlinx)
 }
 
 dependencies {
