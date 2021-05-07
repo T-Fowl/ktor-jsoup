383a4004d2a4c39b7d11cbcac296258ea6176f7d
Thomas <thomasjamesfowler97@gmail.com>
2021-05-07 16:57:17 +1000

    Bumped dokka plugin version (forgot)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.net.URL
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.32"
-    id("org.jetbrains.dokka") version "1.4.30"
+    id("org.jetbrains.dokka") version "1.4.32"
     `maven-publish`
     signing
 }
