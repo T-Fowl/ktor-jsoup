f9eb39159d783ec6afc15a3a9c969aedc58efa47
Thomas <thomasjamesfowler97@gmail.com>
2021-05-07 16:48:03 +1000

    Updated to kotlin 1.4.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import java.net.URI
 import java.net.URL
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.10"
+    id("org.jetbrains.kotlin.jvm") version "1.4.32"
     id("org.jetbrains.dokka") version "1.4.30"
     `maven-publish`
     signing
