2aa3f14f54aa808645102679edd7c88fe63a1c49
Thomas <thomasjamesfowler97@gmail.com>
2021-09-23 13:01:03 +1000

    Removed deprecated KtorExperimentalAPI annotation. Reformatted

--- src/test/kotlin/com/tfowl/ktor/client/features/JsoupFeatureTests.kt
+++ src/test/kotlin/com/tfowl/ktor/client/features/JsoupFeatureTests.kt
@@ -1,13 +1,11 @@
 package com.tfowl.ktor.client.features
 
-import io.ktor.client.HttpClient
-import io.ktor.client.call.NoTransformationFoundException
-import io.ktor.client.engine.mock.MockEngine
-import io.ktor.client.engine.mock.respond
-import io.ktor.client.request.get
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.mock.*
+import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.util.KtorExperimentalAPI
-import io.ktor.util.cio.toByteReadChannel
+import io.ktor.util.cio.*
 import kotlinx.coroutines.runBlocking
 import org.jsoup.nodes.Document
 import org.jsoup.parser.Parser
@@ -17,14 +15,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.io.InputStream
 
-
-@KtorExperimentalAPI
 class JsoupFeatureTests {
 
     private data class Resource(val file: String, val contentType: ContentType)
 
     private fun resource(resource: String): InputStream =
-            JsoupFeatureTests::class.java.getResourceAsStream(resource) ?: error("Missing testing file: $resource")
+        JsoupFeatureTests::class.java.getResourceAsStream(resource) ?: error("Missing testing file: $resource")
 
     private val Url.hostWithPortIfRequired: String get() = if (port == protocol.defaultPort) host else hostWithPort
 
@@ -34,16 +30,18 @@ class JsoupFeatureTests {
         engine {
             addHandler { request ->
                 val resources = mapOf(
-                        "https://example.org/text/html" to Resource("sample.html", ContentType.Text.Html),
-                        "https://example.org/text/xml" to Resource("sample.xml", ContentType.Text.Xml),
-                        "https://example.org/application/xml" to Resource("sample.xml", ContentType.Application.Xml),
-                        "https://example.org/application/rss" to Resource("sample.rss", ContentType.Application.Rss)
+                    "https://example.org/text/html" to Resource("sample.html", ContentType.Text.Html),
+                    "https://example.org/text/xml" to Resource("sample.xml", ContentType.Text.Xml),
+                    "https://example.org/application/xml" to Resource("sample.xml", ContentType.Application.Xml),
+                    "https://example.org/application/rss" to Resource("sample.rss", ContentType.Application.Rss)
                 )
 
                 val resource = resources[request.url.fullUrl] ?: error("Unhandled ${request.url.fullUrl}")
 
-                respond(resource(resource.file).toByteReadChannel(),
-                        headers = headersOf("Content-Type", listOf(resource.contentType.toString())))
+                respond(
+                    resource(resource.file).toByteReadChannel(),
+                    headers = headersOf("Content-Type", listOf(resource.contentType.toString()))
+                )
             }
         }
     }
