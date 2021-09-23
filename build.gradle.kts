f5d13be0b13eaf9cc31d42114f2821f54581af8f
Thomas <thomasjamesfowler97@gmail.com>
2021-09-23 10:52:19 +1000

    Updated kotlin and dokka plugin versions. Kotlin bounded on 1.5.30 to align with ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import java.net.URI
 import java.net.URL
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.5.20"
-    id("org.jetbrains.dokka") version "1.5.0"
+    id("org.jetbrains.kotlin.jvm") version "1.5.30"
+    id("org.jetbrains.dokka") version "1.5.30"
     `maven-publish`
     signing
 }
