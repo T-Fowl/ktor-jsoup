379c7d2a3c21f6c0520bb2c35aee8bb8a8d52589
Thomas <thomasjamesfowler97@gmail.com>
2022-04-05 12:34:31 +1000

    updated client-mock to 1.6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
      */
 
     api("io.ktor:ktor-client:1.6.8")
-    testImplementation("io.ktor:ktor-client-mock:1.6.3")
+    testImplementation("io.ktor:ktor-client-mock:1.6.8")
 
     api("org.jsoup:jsoup:1.14.3")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
