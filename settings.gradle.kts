9f98746a9d5c8f07387e5b182ba1c0b352589e6f
Thomas <thomasjamesfowler97@gmail.com>
2025-07-17 14:57:35 +1000

    Added kotlin version catalogs to build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 include(":example")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("ktorLibs") {
+            from("io.ktor:ktor-version-catalog:3.2.2")
+        }
+    }
+    repositories { mavenCentral() }
+}
